228aad4108e32182058a0c5ba8346a3b0c74edbe
FMP24 <muperezfrancisco@gmail.com>
2023-03-16 16:20:44 +0100

    plantilla-de-inicio

---
+++ LV20GB.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Descripción: Script para configurar un LVM de 20GB estrictamente con 4 discos
+#Autor:
+#Version: 0.1
+#Fecha de creación:
+#Paquetes necesarios:
+
+### DURANTE DESARROLLO; HACED FICHEROS APARTE CON VUESTRAS FUNCIONES, NO ESCRIBAIS AQUÍ ###
