fa5ce7f274da0e9486e0bafa5746b40d3a454186
FMP24 <126695764+FMP24@users.noreply.github.com>
2023-03-21 10:38:18 +0100

    Update 4comprobar.sh

--- 4comprobar.sh
+++ 4comprobar.sh
@@ -5,9 +5,9 @@ function f_comprobar_part {
 j=1
   for i in $(lsblk -n -d -o NAME); do
     if [ "$(lsblk -o NAME,TYPE | awk '$2=="part"{print $1}' | egrep -o "$i")" = "" ]; then
-      return 0
       eval disco$j=$i
       j=$((j+1))
+      return 0
     else
       return 1
     fi
